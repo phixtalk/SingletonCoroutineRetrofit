004dc90fb30a7eff44cb9a96b944dfacbcbebcf2
Ofoefule Christian <ofoefule.c@gmail.com>
2019-11-13 22:35:14 +0100

    modified mainviewmodel class

--- app/src/main/java/com/singletoncoroutineretrofit/MainViewModel.kt
+++ app/src/main/java/com/singletoncoroutineretrofit/MainViewModel.kt
@@ -16,17 +16,19 @@ class MainViewModel: ViewModel() {
     //so that whenever it changes, it will trigger (call and execute) the Repository.getUser(userId)
     //and the _userId can be changed or updated by calling the setUserId()
     //transformation.switchmap method takes in a string and returns a User object, hence the name switchMap
-    val user: LiveData<User> = Transformations
-        .switchMap(_userId){userId ->
+    val user: LiveData<User> = Transformations.switchMap(_userId){userId ->
             Repository.getUser(userId)
         }
 
+    //this is the trigger that calls the above switchmap method
+    //and this trigger is pulled from the mainactivity when the user makes a request with a particular userid value
+    //once the _userId.value changes, the switchmap method is trigger
     fun setUserId(userId: String){
         val update = userId
         if(_userId.value == update){
             return
         }
-        _userId.value = update
+        _userId.value = update//here the value of the _userId object has been updated
     }
 
     fun cancelJobs(){
